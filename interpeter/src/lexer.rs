067fd41f4d0375b102655b1bdecd99fc9b9b0e14
IkBenJur <jurspammer@gmail.com>
2024-04-20 20:55:31 +0200

    Created function to initialize Lexer

--- interpeter/src/lexer.rs
+++ interpeter/src/lexer.rs
@@ -4,3 +4,54 @@ pub struct Lexer {
     read_position: Option<u32>,
     ch: Option<char>,
 }
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        let mut lexer = Self {
+            input,
+            position: None,
+            read_position: None,
+            ch: None,
+        };
+        lexer.read_char();
+        return lexer;
+    }
+
+    fn read_char(&mut self) {
+        if self.position.is_none() {
+            self.position = Some(0);
+            self.read_position = Some(0);
+        }
+
+        if let Some(read_position) = self.read_position {
+            if read_position >= self.input.len() as u32 {
+                self.ch = None;
+            } else {
+                if let Some(read_position) = self.read_position {
+                    let new_char = self.input.chars().nth(read_position as usize).unwrap();
+                    self.ch = Some(new_char);
+                }
+
+                self.position = self.read_position;
+                self.read_position = Some(self.read_position.unwrap() + 1);
+            }
+        }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{lexer::Lexer, token::TokenType};
+
+    #[test]
+    fn test_new_lexer() {
+        let test_lexer = Lexer::new("{}".to_string());
+
+        assert_eq!(test_lexer.input, "{}");
+        assert_eq!(test_lexer.position, Some(0));
+        assert_eq!(test_lexer.read_position, Some(1));
+        assert_eq!(test_lexer.ch, Some('{'))
+    }
+
+}
